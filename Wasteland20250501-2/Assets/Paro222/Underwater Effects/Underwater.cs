a50eb8c3a7ecf92710e91d1dd0c5af112815b98e
TTTyq <TTTyq2021@outlook.com>
2025-05-04 23:19:24 +0100

    Scene underwater update

--- Wasteland20250501-2/Assets/Paro222/Underwater Effects/Underwater.cs
+++ Wasteland20250501-2/Assets/Paro222/Underwater Effects/Underwater.cs
@@ -1,77 +1,112 @@
+using System;
+using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.Rendering;
 using UnityEngine.Rendering.Universal;
+[ExecuteInEditMode]
 
 public class Underwater : ScriptableRendererFeature
 {
     [System.Serializable]
     public class Settings
     {
+        //future settings
         public Material material;
         public RenderPassEvent renderPassEvent = RenderPassEvent.AfterRenderingSkybox;
-        public Color color = new Color(0, 0.5f, 0.8f, 1);
+        public Color color;
         public float FogDensity = 1;
         [Range(0, 1)]
-        public float alpha = 0.5f;
+        public float alpha;
         public float refraction = 0.1f;
         public Texture normalmap;
-        public Vector4 UV = new Vector4(1, 1, 0.2f, 0.1f);
+        public Vector4 UV = new Vector4(1,1,0.2f,0.1f);
     }
 
     public Settings settings = new Settings();
-    UnderwaterPass m_UnderwaterPass;
-
-    public override void Create()
+    class Pass : ScriptableRenderPass
     {
-        m_UnderwaterPass = new UnderwaterPass();
-        m_UnderwaterPass.renderPassEvent = settings.renderPassEvent;
-    }
+        public Settings settings;
+        private RenderTargetIdentifier source;
+        RenderTargetHandle tempTexture;
 
-    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
-    {
-        if (settings.material == null)
-            return;
+        private string profilerTag;
 
-        m_UnderwaterPass.Setup(settings);
-        renderer.EnqueuePass(m_UnderwaterPass);
-    }
-
-    class UnderwaterPass : ScriptableRenderPass
-    {
-        private ProfilingSampler m_ProfilingSampler = new ProfilingSampler("Underwater");
-        private Settings m_Settings;
-        private Material m_Material;
-
-        public void Setup(Settings settings)
+        public void Setup(RenderTargetIdentifier source)
         {
-            m_Settings = settings;
-            m_Material = settings.material;
+            this.source = source;
+        }
+
+        public Pass(string profilerTag)
+        {
+            this.profilerTag = profilerTag;
+        }
+
+        public override void Configure(CommandBuffer cmd, RenderTextureDescriptor cameraTextureDescriptor)
+        {
+            cmd.GetTemporaryRT(tempTexture.id, cameraTextureDescriptor);
+            ConfigureTarget(tempTexture.Identifier());
+            ConfigureClear(ClearFlag.All, Color.black);
         }
 
         public override void Execute(ScriptableRenderContext context, ref RenderingData renderingData)
         {
-            if (m_Material == null)
-                return;
+            CommandBuffer cmd = CommandBufferPool.Get(profilerTag);
+            cmd.Clear();
 
-            CommandBuffer cmd = CommandBufferPool.Get();
-
-            using (new ProfilingScope(cmd, m_ProfilingSampler))
+            //it is very important that if something fails our code still calls 
+            //CommandBufferPool.Release(cmd) or we will have a HUGE memory leak
+            try
             {
-                // 设置材质属性
-                m_Material.SetFloat("_FogDensity", m_Settings.FogDensity);
-                m_Material.SetFloat("_alpha", m_Settings.alpha);
-                m_Material.SetColor("_color", m_Settings.color);
-                m_Material.SetTexture("_NormalMap", m_Settings.normalmap);
-                m_Material.SetFloat("_refraction", m_Settings.refraction);
-                m_Material.SetVector("_normalUV", m_Settings.UV);
+                //here we set out material properties
+                //...
+                settings.material.SetFloat("_FogDensity", settings.FogDensity);
 
-                // 使用RenderingUtils.fullscreenMesh绘制全屏四边形
-                // 这里不需要设置源目标，因为材质会自动采样_CameraOpaqueTexture
-                cmd.DrawMesh(RenderingUtils.fullscreenMesh, Matrix4x4.identity, m_Material);
+                settings.material.SetFloat("_alpha", settings.alpha);
+
+                settings.material.SetColor("_color", settings.color);
+
+                settings.material.SetTexture("_NormalMap", settings.normalmap);
+
+                settings.material.SetFloat("_refraction", settings.refraction);
+
+                settings.material.SetVector("_normalUV", settings.UV);
+
+                //never use a Blit from source to source, as it only works with MSAA
+                // enabled and the scene view doesnt have MSAA,
+                // so the scene view will be pure black
+
+                cmd.Blit(source, tempTexture.Identifier());
+                cmd.Blit(tempTexture.Identifier(), source, settings.material, 0);
+
+                context.ExecuteCommandBuffer(cmd);
             }
-
-            context.ExecuteCommandBuffer(cmd);
+            catch
+            {
+                Debug.LogError("Error");
+            }
+            cmd.Clear();
             CommandBufferPool.Release(cmd);
         }
     }
+
+    Pass pass;
+    RenderTargetHandle renderTextureHandle;
+    public override void Create()
+    {
+        pass = new Pass("Underwater Effects");
+        name = "Underwater Effects";
+        pass.settings = settings;
+        pass.renderPassEvent = settings.renderPassEvent;
+    }
+    public override void SetupRenderPasses(ScriptableRenderer renderer, in RenderingData renderingData)
+    {
+        var cameraColorTargetIdent = renderer.cameraColorTarget;
+        pass.Setup(cameraColorTargetIdent);
+    }
+
+    public override void AddRenderPasses(ScriptableRenderer renderer, ref RenderingData renderingData)
+    {
+        renderer.EnqueuePass(pass);
+    }
 }
+
--- Wasteland20250501-2/Assets/Paro222/Underwater Effects/UnderwaterEffects.shader
+++ Wasteland20250501-2/Assets/Paro222/Underwater Effects/UnderwaterEffects.shader
@@ -3,93 +3,70 @@ Shader "Paro222/UnderwaterEffects"
     Properties
     {
         _MainTex ("Texture", 2D) = "white" {}
-        _NormalMap ("Normal Map", 2D) = "bump" {}
-        _color ("Water Color", Color) = (0, 0.5, 0.8, 1)
-        _FogDensity ("Fog Density", Range(0, 10)) = 1
-        _alpha ("Alpha", Range(0, 1)) = 0.5
-        _refraction ("Refraction", Range(0, 1)) = 0.1
-        _normalUV ("Normal UV", Vector) = (1, 1, 0.2, 0.1)
     }
-    
     SubShader
     {
-        Tags { "RenderType"="Opaque" "RenderPipeline" = "UniversalPipeline" }
+        Tags { "RenderType"="Opaque" }
         LOD 100
-        ZTest Always
-        ZWrite Off
-        Cull Off
-        
+
         Pass
         {
-            Name "UnderwaterPass"
-            
-            HLSLPROGRAM
+            CGPROGRAM
             #pragma vertex vert
             #pragma fragment frag
-            
-            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/Core.hlsl"
-            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/DeclareOpaqueTexture.hlsl"
-            #include "Packages/com.unity.render-pipelines.universal/ShaderLibrary/DeclareDepthTexture.hlsl"
-            
-            struct Attributes
+
+            #include "UnityCG.cginc"
+
+            struct appdata
             {
-                float4 positionOS : POSITION;
+                float4 vertex : POSITION;
                 float2 uv : TEXCOORD0;
-                UNITY_VERTEX_INPUT_INSTANCE_ID
             };
-            
-            struct Varyings
+
+            struct v2f
             {
                 float2 uv : TEXCOORD0;
-                float4 positionCS : SV_POSITION;
-                UNITY_VERTEX_OUTPUT_STEREO
+                float4 vertex : SV_POSITION;
             };
-            
-            TEXTURE2D(_NormalMap);
-            SAMPLER(sampler_NormalMap);
-            
-            float4 _color;
+
+            half remap(half x, half t1, half t2, half s1, half s2)
+            {
+                return (x - t1) / (t2 - t1) * (s2 - s1) + s1;
+            }
+
+            sampler2D _MainTex;
+            sampler2D _NormalMap;
+            float4 _normalUV;
+            float4 _MainTex_ST;
+            fixed4 _color;
             float _FogDensity;
             float _alpha;
             float _refraction;
-            float4 _normalUV;
-            
-            Varyings vert(Attributes input)
+            sampler2D_float _CameraDepthTexture;
+
+            v2f vert (appdata v)
             {
-                Varyings output = (Varyings)0;
-                UNITY_SETUP_INSTANCE_ID(input);
-                UNITY_INITIALIZE_VERTEX_OUTPUT_STEREO(output);
-                
-                output.positionCS = TransformObjectToHClip(input.positionOS.xyz);
-                output.uv = input.uv;
-                return output;
+                v2f o;
+                o.vertex = UnityObjectToClipPos(v.vertex);
+                o.uv = TRANSFORM_TEX(v.uv, _MainTex);
+                return o;
             }
-            
-            half4 frag(Varyings input) : SV_Target
+
+            fixed4 frag (v2f i) : SV_Target
             {
-                UNITY_SETUP_STEREO_EYE_INDEX_POST_VERTEX(input);
+                // sample the texture
+                fixed3 normalmap = UnpackNormal(tex2D(_NormalMap, i.uv * _normalUV.xy + _normalUV.zw * _Time.y));
+
+                float depth_tex = UNITY_SAMPLE_DEPTH(tex2D(_CameraDepthTexture, i.uv + normalmap * _refraction * 0.01));
+                float depth = Linear01Depth(depth_tex) * Linear01Depth(depth_tex);
+
+                float fogAmount = 1.0 - exp(-_FogDensity * depth);
                 
-                // 采样法线贴图
-                float3 normalMap = UnpackNormal(SAMPLE_TEXTURE2D(_NormalMap, sampler_NormalMap, 
-                                   input.uv * _normalUV.xy + _normalUV.zw * _Time.y));
-                
-                // 扭曲UV坐标
-                float2 distortedUV = input.uv + normalMap.xy * _refraction * 0.01;
-                
-                // 采样场景颜色（从不透明纹理，而非_MainTex）
-                half4 sceneColor = SampleSceneColor(distortedUV);
-                
-                // 采样场景深度
-                float sceneDepth = SampleSceneDepth(distortedUV);
-                float linearDepth = LinearEyeDepth(sceneDepth, _ZBufferParams);
-                
-                // 计算水下雾效
-                float fogFactor = 1.0 - exp(-_FogDensity * linearDepth);
-                
-                // 混合水下颜色
-                return lerp(sceneColor, _color, saturate(fogFactor + _alpha));
+                fixed4 col = tex2D(_MainTex, i.uv + normalmap * _refraction * 0.01);
+
+                return lerp(col, _color, saturate(fogAmount * 1000 + _alpha));
             }
-            ENDHLSL
+            ENDCG
         }
     }
 }
